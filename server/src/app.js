e8cf65192b4ec4e737a5ebfa39a8ade67d52c5c8
Lucky Patil <luckypatil577R@gmail.com>
2026-01-16 23:14:13 +0530

    Internship APIs ,Project Registration APIs , Admin fetch APIs working

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import internshipRoutes from "./routes/internship.routes.js";
+import projectRoutes from "./routes/project.routes.js";
 
 dotenv.config();
 connectDB();
@@ -12,7 +13,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/internships", internshipRoutes);  
+app.use("/api/internships", internshipRoutes);
+app.use("/api/projects", projectRoutes);  
 
 app.get("/", (req, res) => {
   res.send("MakeTechBerry Server Running 🚀");
